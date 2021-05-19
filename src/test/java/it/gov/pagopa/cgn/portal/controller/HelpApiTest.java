9c11c50224fabed60b71944eee4de36dd5b7d902
Luca Tronchin <ltronky@gmail.com>
2021-05-19 15:36:13 +0200

    Add Recaptcha challenge on public help API

--- src/test/java/it/gov/pagopa/cgn/portal/controller/HelpApiTest.java
+++ src/test/java/it/gov/pagopa/cgn/portal/controller/HelpApiTest.java
@@ -5,26 +5,52 @@ import it.gov.pagopa.cgn.portal.IntegrationAbstractTest;
 import it.gov.pagopa.cgn.portal.TestUtils;
 import it.gov.pagopa.cgn.portal.model.AgreementEntity;
 import it.gov.pagopa.cgn.portal.model.ProfileEntity;
+import it.gov.pagopa.cgn.portal.recaptcha.GoogleRecaptchaResponse;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.TestConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
 import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
+import static org.mockito.ArgumentMatchers.*;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.log;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
+@RunWith(SpringRunner.class)
 @AutoConfigureMockMvc(addFilters = false)
-public class HelpApiTest extends IntegrationAbstractTest {
+@Import(HelpApiTest.IntegrationAbstractTestConfiguration.class)
+class HelpApiTest extends IntegrationAbstractTest {
+
+    @Autowired
+    private RestTemplate restTemplateMock;
 
     @Autowired
     private MockMvc mockMvc;
 
     private AgreementEntity agreement;
+
+    @TestConfiguration
+    static class IntegrationAbstractTestConfiguration {
+
+        @Bean
+        @Primary
+        public RestTemplate getRestTemplateBean() {
+            return mock(RestTemplate.class);
+        }
+    }
 
     @BeforeEach
     void init() {
@@ -37,12 +63,34 @@ public class HelpApiTest extends IntegrationAbstractTest {
 
         it.gov.pagopa.cgnonboardingportal.publicapi.model.HelpRequest helpRequest = TestUtils.createSamplePublicApiHelpRequest();
 
+        GoogleRecaptchaResponse response = new GoogleRecaptchaResponse();
+        response.setSuccess(true);
+        when(restTemplateMock.postForObject(anyString(), any(), eq(GoogleRecaptchaResponse.class))).thenReturn(response);
+
         this.mockMvc.perform(
                 post(TestUtils.PUBLIC_HELP_CONTROLLER_PATH)
                         .contentType(MediaType.APPLICATION_JSON).content(TestUtils.getJson(helpRequest))
         )
                 .andDo(log())
                 .andExpect(status().isNoContent());
+    }
+
+
+    @Test
+    void Send_SendPublicHelpRequestWithInvalidToken_BadRequest() throws Exception {
+
+        it.gov.pagopa.cgnonboardingportal.publicapi.model.HelpRequest helpRequest = TestUtils.createSamplePublicApiHelpRequest();
+
+        GoogleRecaptchaResponse response = new GoogleRecaptchaResponse();
+        response.setSuccess(false);
+        when(restTemplateMock.postForObject(anyString(), any(), eq(GoogleRecaptchaResponse.class))).thenReturn(response);
+
+        this.mockMvc.perform(
+                post(TestUtils.PUBLIC_HELP_CONTROLLER_PATH)
+                        .contentType(MediaType.APPLICATION_JSON).content(TestUtils.getJson(helpRequest))
+        )
+                .andDo(log())
+                .andExpect(status().isBadRequest());
     }
 
     @Test
