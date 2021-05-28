7c38855200b36197652e8b3b76893ee2660c0ae3
Carmelo Calabro <carmelo.calabro@bitrock.it>
2021-05-28 10:03:34 +0200

    Revert "Added indexes to improve performance of search API". This commit will be pushed into different branch

--- src/main/resources/db/migration/V017__add_index_on_materialized_view.sql
+++
@@ -1,6 +0,0 @@
---this extension is used by GIN index that allow using the index when a like is used
-CREATE EXTENSION IF NOT EXISTS pg_trgm;
-CREATE INDEX IF NOT EXISTS idx_online_merchant_search_name ON online_merchant USING gin (searchable_name gin_trgm_ops);
-CREATE INDEX IF NOT EXISTS idx_offline_merchant_search_name ON offline_merchant USING gin (searchable_name gin_trgm_ops);
-
-CREATE INDEX IF NOT EXISTS idx_offline_merchant_lat_lon on offline_merchant (latitude, longitude);
