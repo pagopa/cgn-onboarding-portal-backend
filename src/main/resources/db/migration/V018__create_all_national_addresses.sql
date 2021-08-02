16bc91f2b13cef9595acc5e9cb395af8647e293c
AleDore <alessio85.dore@gmail.com>
2021-08-02 10:48:29 +0200

    fix migration script

--- src/main/resources/db/migration/V018__create_all_national_addresses.sql
+++ src/main/resources/db/migration/V018__create_all_national_addresses.sql
@@ -1,4 +1,4 @@
 ALTER TABLE profile ADD COLUMN all_national_addresses BOOLEAN NULL;
 
-ALTER TABLE address ALTER COLUMN latitude SET NULL;
-ALTER TABLE address ALTER COLUMN longitude SET NULL;
+ALTER TABLE address ALTER COLUMN latitude DROP NOT NULL;
+ALTER TABLE address ALTER COLUMN longitude DROP NOT NULL;
