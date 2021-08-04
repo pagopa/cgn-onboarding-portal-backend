67a4a2fdf27a5bfbb291b731e92f873ab7edb865
AleDore <alessio85.dore@gmail.com>
2021-08-04 12:56:22 +0200

    hotfix update all_national_addresses default value

--- src/main/resources/db/migration/V022__create_all_national_addresses.sql
+++ src/main/resources/db/migration/V022__create_all_national_addresses.sql
@@ -2,3 +2,6 @@ ALTER TABLE profile ADD COLUMN all_national_addresses BOOLEAN NULL;
 
 ALTER TABLE address ALTER COLUMN latitude DROP NOT NULL;
 ALTER TABLE address ALTER COLUMN longitude DROP NOT NULL;
+
+UPDATE profile SET all_national_addresses = true WHERE sales_channel IN ('BOTH', 'OFFLINE') AND profile_k NOT IN (SELECT profile_fk from address);
+UPDATE profile SET all_national_addresses = false WHERE sales_channel IN ('BOTH', 'OFFLINE') AND profile_k IN (SELECT profile_fk from address);
