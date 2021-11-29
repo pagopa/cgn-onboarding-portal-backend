6bf64a3a2ea7614ede1aadf6306e627d46b84e67
Michael Disaro <michael.disaro@gmail.com>
2021-11-29 17:23:40 +0100

    Retry for async bucket of codes loading

--- src/main/java/it/gov/pagopa/cgn/portal/util/BucketLoadUtils.java
+++ src/main/java/it/gov/pagopa/cgn/portal/util/BucketLoadUtils.java
@@ -1,9 +1,12 @@
 package it.gov.pagopa.cgn.portal.util;
 
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Retryable;
 import org.springframework.scheduling.annotation.Async;
 import org.springframework.stereotype.Component;
 
 import it.gov.pagopa.cgn.portal.service.BucketService;
+import org.springframework.util.backoff.BackOff;
 
 @Component
 public class BucketLoadUtils {
@@ -15,6 +18,7 @@ public class BucketLoadUtils {
     }
 
     @Async("threadPoolTaskExecutor")
+    @Retryable(maxAttempts = 3, backoff = @Backoff(delay = 500, multiplier = 1.5))
     public void storeCodesBucket(Long discountId) {
         bucketService.setRunningBucketLoad(discountId);
         bucketService.performBucketLoad(discountId);
