ab33c24071d589bfc325650f59e3f93974da1648
Michael Disaro <michael.disaro@gmail.com>
2022-01-19 15:47:20 +0100

    tested enums

--- src/main/java/it/gov/pagopa/cgn/portal/model/NotificationEntity.java
+++ src/main/java/it/gov/pagopa/cgn/portal/model/NotificationEntity.java
@@ -1,10 +1,11 @@
 package it.gov.pagopa.cgn.portal.model;
 
 import lombok.Data;
-import lombok.EqualsAndHashCode.Exclude;
-import lombok.ToString;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
