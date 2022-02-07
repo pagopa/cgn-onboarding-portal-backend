577f2dc0e500980d64e6da790744348011f12bd6
Michael Disaro <michael.disaro@gmail.com>
2022-02-07 17:11:06 +0100

    removed group bys

--- src/main/resources/db/migration/R__create_approved_agreements_view.sql
+++ src/main/resources/db/migration/R__create_approved_agreements_view.sql
@@ -1,6 +1,13 @@
 DROP VIEW IF EXISTS approved_agreements;
 
 CREATE VIEW approved_agreements AS
+WITH discounts_counter AS (
+    SELECT a.agreement_k,
+           COUNT(d.*) as published_discounts
+    FROM agreement a
+             LEFT JOIN discount d ON (d.agreement_fk = a.agreement_k and d.state = 'PUBLISHED')
+    GROUP BY a.agreement_k
+)
 SELECT a.agreement_k,
        a.information_last_update_date,
        a.start_date,
@@ -8,9 +15,7 @@ SELECT a.agreement_k,
        a.assignee,
        a.request_approval_time,
        p.full_name,
-       COUNT(d.*) as published_discounts
+       c.published_discounts
 FROM agreement a
          JOIN profile p ON (a.agreement_k = p.agreement_fk)
-         LEFT JOIN discount d ON (d.agreement_fk = a.agreement_k and d.state = 'PUBLISHED')
-GROUP BY a.agreement_k, a.information_last_update_date, a.start_date, a.state, a.assignee, a.request_approval_time,
-         p.full_name
+         JOIN discounts_counter c ON (a.agreement_k = c.agreement_k)
