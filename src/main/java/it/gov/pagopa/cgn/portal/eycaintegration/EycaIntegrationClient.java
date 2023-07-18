3c619dcd4b682ff87f86185cdf38162261a87c94
AndreaRovereMI <andrea.rovere@dgsspa.com>
2023-07-18 10:50:28 +0200

    Commit 18072023

---
+++ src/main/java/it/gov/pagopa/cgn/portal/eycaintegration/EycaIntegrationClient.java
@@ -0,0 +1,50 @@
+package it.gov.pagopa.cgn.portal.eycaintegration;
+
+
+import it.gov.pagopa.cgnonboardingportal.eycaintegration.model.DiscountRequestEycaIntegration;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+import java.util.Map;
+
+@Component
+public class EycaIntegrationClient {
+
+   /* public void addPet(DiscountRequestEycaIntegration body,  String accessToken,) throws ApiException {
+        if (accessToken == null) {
+            throw new ApiException(
+                    400, "Missing the required parameter 'accessToken' when calling devicelist");
+        }
+
+        Object postBody = body;
+
+        // create path and map variables
+        String path = "/pet".replaceAll("\\{format\\}", "json");
+
+        // query params
+        List<Pair> queryParams = new ArrayList<Pair>();
+        Map<String, String> headerParams = new HashMap<String, String>();
+        Map<String, Object> formParams = new HashMap<String, Object>();
+
+        final String[] accepts = {"application/json", "application/xml"};
+        final String accept = apiClient.selectHeaderAccept(accepts);
+
+        final String[] contentTypes = {"application/json", "application/xml"};
+        final String contentType = apiClient.selectHeaderContentType(contentTypes);
+
+        String[] authNames = new String[] {"petstore_auth"};
+
+        apiClient.invokeAPI(
+                path,
+                "POST",
+                queryParams,
+                postBody,
+                headerParams,
+                formParams,
+                accept,
+                contentType,
+                authNames,
+                null);
+    }
+*/
+}
--- src/main/java/it/gov/pagopa/cgn/portal/service/EycaIntegrationService.java
+++ src/main/java/it/gov/pagopa/cgn/portal/service/EycaIntegrationService.java
@@ -3,10 +3,13 @@ package it.gov.pagopa.cgn.portal.service;
 
 import it.gov.pagopa.cgn.portal.config.ConfigProperties;
 import it.gov.pagopa.cgnonboardingportal.eycaintegration.api.EycaIntegrationApi;
+import it.gov.pagopa.cgnonboardingportal.eycaintegration.client.ApiClient;
 import it.gov.pagopa.cgnonboardingportal.eycaintegration.model.ApiResponseEycaIntegration;
 import it.gov.pagopa.cgnonboardingportal.eycaintegration.model.DiscountRequestEycaIntegration;
 import it.gov.pagopa.cgnonboardingportal.eycaintegration.model.InlineResponse200EycaIntegration;
 import org.springframework.stereotype.Service;
+
+import javax.annotation.PostConstruct;
 
 @Service
 public class EycaIntegrationService {
@@ -27,5 +30,37 @@ public class EycaIntegrationService {
             DiscountRequestEycaIntegration discountRequestEycaIntegration) {
         return eycaIntegrationApi.createDiscount(discountRequestEycaIntegration);
     }
+
+
+    @PostConstruct
+    public void init() {
+    /*    log.info("INIT " + this.getClass().getSimpleName());
+        MAX_RETRIES = Integer.parseInt(configBean.getConfig(Configurations.NODO_MAX_RETRIES));
+        RETRY_DELAY_MS = Integer.parseInt(configBean.getConfig(Configurations.NODO_RETRY_DELAY_MS));
+        nodoApi = new NodoApi(new ApiClient()
+                .setBasePath(getNodoBasePath())
+                .setConnectTimeout(Integer.parseInt(configBean.getConfig(Configurations.NODO_TIMEOUT_MS))));
+        log.info("END INIT " + this.getClass().getSimpleName());*/
+    }
+
+    public ApiResponseEycaIntegration createDiscountWithAuthorization(
+            String username, String password, DiscountRequestEycaIntegration discountRequestEycaIntegration) {
+
+        ApiClient apiClient = eycaIntegrationApi.getApiClient();
+        apiClient.setBasePath("");
+
+        // Eseguire l'autenticazione
+        InlineResponse200EycaIntegration authResponse = eycaIntegrationApi.authentication(username, password);
+
+        // Ottenere il cookie di sessione dalla risposta di autenticazione
+        String sessionId = authResponse.getSessionId();
+
+        // Aggiungere il cookie di sessione alle richieste successive
+       // apiClient.getCookieParams().put("session-id", Collections.singletonList(sessionId));
+
+        // Effettuare la chiamata a createDiscount con l'autenticazione tramite cookie
+        return eycaIntegrationApi.createDiscount(discountRequestEycaIntegration);
+    }
+
 
 }
