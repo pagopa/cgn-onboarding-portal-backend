95e837e25d3fdcb7c07b51219e1b42d57f540dcb
AndreaRovereMI <andrea.rovere@dgsspa.com>
2023-08-23 10:38:46 +0200

    reverted SprinApllication run

--- src/main/java/it/gov/pagopa/cgn/portal/CGNOnboardingPortal.java
+++ src/main/java/it/gov/pagopa/cgn/portal/CGNOnboardingPortal.java
@@ -1,38 +1,13 @@
 package it.gov.pagopa.cgn.portal;
 
-import it.gov.pagopa.cgn.portal.service.EycaExportService;
-import it.gov.pagopa.cgnonboardingportal.eycadataexport.model.DataExportEyca;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ApplicationContext;
-
-import java.util.Arrays;
-import java.util.List;
-
 
 @SpringBootApplication
 public class CGNOnboardingPortal {
 
     public static void main(String[] args) {
-
-       ApplicationContext applicationContext =SpringApplication.run(CGNOnboardingPortal.class, args);
-        EycaExportService service = applicationContext.getBean(EycaExportService.class);
-        DataExportEyca requestEycaDataExport = new DataExportEyca();
-        requestEycaDataExport.setLive(0);
-        requestEycaDataExport.setEmail("email@email.com");
-        requestEycaDataExport.setName("name");
-        requestEycaDataExport.setLocalId("localId_007");
-        requestEycaDataExport.setPhone("944954893");
-        requestEycaDataExport.setNameLocal("nameLocal");
-        requestEycaDataExport.setVendor("vendor");
-        requestEycaDataExport.setText("dudatext");
-        service.createDiscountWithAuthorization(requestEycaDataExport, "json");
-
-
-
+        SpringApplication.run(CGNOnboardingPortal.class, args);
     }
-
-
-
 
 }
