165cb0782ca7568458642a95e751c8c40c1a07fd
aforcuti <alessandro.forcuti@gsspa.com>
2024-06-18 11:19:01 +0200

    Updated version of application insights and fix class name

--- src/main/java/it/gov/pagopa/cgn/portal/config/GlobalBindingHandler.java
+++ src/main/java/it/gov/pagopa/cgn/portal/config/GlobalBindingHandler.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.WebDataBinder;
 import org.springframework.web.bind.annotation.*;
 
 @ControllerAdvice
-public class GlobalBindingHendler {
+public class GlobalBindingHandler {
 	@InitBinder
 	public void initBinder(WebDataBinder binder) {
 		binder.registerCustomEditor(String.class, new StringTrimmerEditor(true));
