5cac794b0597fab92ec088528c5b64ae1d22691b
Michael Disarò <michael.disaro@pagopa.it>
2025-02-19 10:27:19 +0100

    fixed imports

--- src/main/java/it/gov/pagopa/cgn/portal/model/DiscountBucketCodeEntity.java
+++ src/main/java/it/gov/pagopa/cgn/portal/model/DiscountBucketCodeEntity.java
@@ -10,7 +10,6 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.io.Serializable;
-import java.time.LocalDateTime;
 import java.time.OffsetDateTime;
 
 @Entity
