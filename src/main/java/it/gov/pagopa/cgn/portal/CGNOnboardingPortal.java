a2f4a4051ada2e1639b2c708dd1ac10d9437118c
aforcuti <alessandro.forcuti@gsspa.com>
2025-02-21 11:18:15 +0100

    Added profile dev

--- src/main/java/it/gov/pagopa/cgn/portal/CGNOnboardingPortal.java
+++ src/main/java/it/gov/pagopa/cgn/portal/CGNOnboardingPortal.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Profile;
 
 import java.util.Arrays;
 
@@ -14,13 +15,13 @@ public class CGNOnboardingPortal {
     public static void main(String[] args) {
         SpringApplication.run(CGNOnboardingPortal.class, args);
     }
-/*
+
     @Bean
+    @Profile("dev")
     public CommandLineRunner printBeans(ApplicationContext ctx) {
         return args -> {
-            System.out.println("Beans registrati:");
+            System.out.println("Registered beans:");
             Arrays.stream(ctx.getBeanDefinitionNames()).sorted().forEach(System.out::println);
         };
     }
-*/
 }
--- src/main/java/it/gov/pagopa/cgn/portal/config/OpenAPIConfig.java
+++ src/main/java/it/gov/pagopa/cgn/portal/config/OpenAPIConfig.java
@@ -9,8 +9,10 @@ import io.swagger.v3.oas.models.security.SecurityScheme.Type;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 
 @Configuration
+@Profile("dev")
 public class OpenAPIConfig {
 
     @Value("${cgn.role.header}")
