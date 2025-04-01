3c44883064dbbfe455c47d67ac65bedd8470c7c1
michaeldisaro <michael.disaro@gmail.com>
2025-04-01 15:33:41 +0200

    fixed devops

--- src/main/java/it/gov/pagopa/cgn/portal/controller/PublicSessionApiController.java
+++ src/main/java/it/gov/pagopa/cgn/portal/controller/PublicSessionApiController.java
@@ -9,11 +9,13 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 
 import static java.util.stream.Collectors.joining;
 
+@RestController
 @Slf4j
 public class PublicSessionApiController
         implements SessionApi {
