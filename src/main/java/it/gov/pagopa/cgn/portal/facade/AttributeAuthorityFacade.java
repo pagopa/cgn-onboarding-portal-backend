761f2a26b221f449b86d1c88d029301bb95ab373
michaeldisaro <michael.disaro@gmail.com>
2025-04-08 12:06:55 +0200

    return empty organization list if error in getting referent

--- src/main/java/it/gov/pagopa/cgn/portal/facade/AttributeAuthorityFacade.java
+++ src/main/java/it/gov/pagopa/cgn/portal/facade/AttributeAuthorityFacade.java
@@ -12,6 +12,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 import java.util.HashMap;
 import java.util.List;
@@ -28,25 +29,30 @@ public class AttributeAuthorityFacade {
 
     public ResponseEntity<Organizations> getOrganizations() {
         String operatorFiscalCode = CGNUtils.getJwtOperatorFiscalCode();
-        List<CompanyAttributeAuthority> companies = attributeAuthorityService.getAgreementOrganizations(
-                operatorFiscalCode);
+        try {
+            List<CompanyAttributeAuthority> companies = attributeAuthorityService.getAgreementOrganizations(
+                    operatorFiscalCode);
 
-        Organizations organizations = new Organizations(companies.stream().map(company -> {
-            HashMap<String, String> claims = new HashMap<>();
-            claims.put(JwtClaims.FIRST_NAME.getCode(), CGNUtils.getJwtOperatorFirstName());
-            claims.put(JwtClaims.LAST_NAME.getCode(), CGNUtils.getJwtOperatorLastName());
-            claims.put(JwtClaims.FISCAL_CODE.getCode(), CGNUtils.getJwtOperatorFiscalCode());
-            claims.put(JwtClaims.ROLE.getCode(), CgnUserRoleEnum.OPERATOR.getCode());
-            claims.put(JwtClaims.ORGANIZATION_FISCAL_CODE.getCode(), company.getFiscalCode());
-            String organizationToken = jwtUtils.buildJwtToken(claims);
-            Organization organization = new Organization();
-            organization.setOrganizationName(company.getOrganizationName());
-            organization.setEmail(company.getPec());
-            organization.setOrganizationFiscalCode(company.getFiscalCode());
-            organization.setToken(organizationToken);
-            return organization;
-        }).toList());
+            Organizations organizations = new Organizations(companies.stream().map(company -> {
+                HashMap<String, String> claims = new HashMap<>();
+                claims.put(JwtClaims.FIRST_NAME.getCode(), CGNUtils.getJwtOperatorFirstName());
+                claims.put(JwtClaims.LAST_NAME.getCode(), CGNUtils.getJwtOperatorLastName());
+                claims.put(JwtClaims.FISCAL_CODE.getCode(), CGNUtils.getJwtOperatorFiscalCode());
+                claims.put(JwtClaims.ROLE.getCode(), CgnUserRoleEnum.OPERATOR.getCode());
+                claims.put(JwtClaims.ORGANIZATION_FISCAL_CODE.getCode(), company.getFiscalCode());
+                String organizationToken = jwtUtils.buildJwtToken(claims);
+                Organization organization = new Organization();
+                organization.setOrganizationName(company.getOrganizationName());
+                organization.setEmail(company.getPec());
+                organization.setOrganizationFiscalCode(company.getFiscalCode());
+                organization.setToken(organizationToken);
+                return organization;
+            }).toList());
 
-        return ResponseEntity.ok(organizations);
+            return ResponseEntity.ok(organizations);
+        } catch (HttpClientErrorException e) {
+            // if we have exception we return empty list
+            return ResponseEntity.ok(new Organizations());
+        }
     }
 }
