9ec6ad2e4eafbee2f740ce8591e6f6a25da40aa4
aforcuti <alessandro.forcuti@gsspa.com>
2025-05-23 12:02:01 +0200

    Removed unused import

--- src/test/java/it/gov/pagopa/cgn/portal/service/DiscountServiceTest.java
+++ src/test/java/it/gov/pagopa/cgn/portal/service/DiscountServiceTest.java
@@ -30,7 +30,6 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.concurrent.*;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 @SpringBootTest
