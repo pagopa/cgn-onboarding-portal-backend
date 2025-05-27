d6666aca81024edeaa781f2e9ffcb15ad25c31d7
michaeldisaro <michael.disaro@gmail.com>
2025-05-27 18:17:51 +0200

    added another index

--- src/main/resources/db/migration/V054__add_indexes_discount_bucket_code.sql
+++ src/main/resources/db/migration/V054__add_indexes_discount_bucket_code.sql
@@ -1,1 +1,2 @@
 CREATE INDEX IF NOT EXISTS discount_bucket_discount_fk_idx ON discount_bucket_code (discount_fk);
+CREATE INDEX IF NOT EXISTS discount_bucket_discount_fk_used_idx ON discount_bucket_code (discount_fk, used);
