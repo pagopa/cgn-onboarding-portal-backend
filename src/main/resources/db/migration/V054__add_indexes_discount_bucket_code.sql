a7fcfec688afe69424c4675b1dcdcd0d95a43e99
michaeldisaro <michael.disaro@gmail.com>
2025-05-27 17:47:36 +0200

    fixed index creation to allow manual creation

--- src/main/resources/db/migration/V054__add_indexes_discount_bucket_code.sql
+++ src/main/resources/db/migration/V054__add_indexes_discount_bucket_code.sql
@@ -1,1 +1,1 @@
-CREATE INDEX discount_bucket_discount_fk_idx ON discount_bucket_code (discount_fk);
+CREATE INDEX IF NOT EXISTS discount_bucket_discount_fk_idx ON discount_bucket_code (discount_fk);
