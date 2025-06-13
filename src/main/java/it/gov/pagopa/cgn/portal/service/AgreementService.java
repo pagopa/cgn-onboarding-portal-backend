b57c778995e5742f39a3ba205c2dfec2aa93186c
aforcuti <alessandro.forcuti@gsspa.com>
2025-06-13 10:04:20 +0200

    fix by sonar

--- src/main/java/it/gov/pagopa/cgn/portal/service/AgreementService.java
+++ src/main/java/it/gov/pagopa/cgn/portal/service/AgreementService.java
@@ -23,7 +23,6 @@ import java.time.LocalDate;
 import java.time.OffsetDateTime;
 import java.util.List;
 import java.util.Optional;
-import java.util.stream.Collectors;
 
 @Service
 public class AgreementService
@@ -151,7 +150,7 @@ public class AgreementService
             discounts = discounts.stream().filter(d -> !DiscountStateEnum.DRAFT.equals(d.getState()) // not draft
                                                        && LocalDate.now()
                                                                    .isBefore(d.getEndDate().plusDays(1))) // not expired
-                                 .collect(Collectors.toList());
+                    .toList();
             agreementEntity.setDiscountList(discounts);
         }
         agreementEntity.setDocumentList(documentService.getAllDocuments(agreementId,
