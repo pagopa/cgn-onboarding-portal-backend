0aeebf56286c1eeac3b0ef25aa5444a21d9b99d5
Alessandro Forcuti <forcutialessandro@gmail.com>
2026-01-30 10:22:21 +0100

    Replaced  CHARACTER VARYING with VARCHAR

--- src/main/resources/db/migration/V062__create_attribute_authority_tables.sql
+++ src/main/resources/db/migration/V062__create_attribute_authority_tables.sql
@@ -1,17 +1,17 @@
 CREATE TABLE organizations (
-    fiscal_code CHARACTER VARYING(16) NOT NULL PRIMARY KEY,
-    name CHARACTER VARYING(100) NOT NULL,
-    pec CHARACTER VARYING(100) NOT NULL,
+    fiscal_code VARCHAR(16) NOT NULL PRIMARY KEY,
+    name VARCHAR(100) NOT NULL,
+    pec VARCHAR(100) NOT NULL,
     inserted_at TIMESTAMP WITH TIME ZONE NOT NULL
 );
 
 CREATE TABLE referents (
-    fiscal_code CHARACTER VARYING(16) NOT NULL PRIMARY KEY
+    fiscal_code VARCHAR(16) NOT NULL PRIMARY KEY
 );
 
 CREATE TABLE organizations_referents (
-    organization_fiscal_code CHARACTER VARYING(16) NOT NULL,
-    referent_fiscal_code CHARACTER VARYING(16) NOT NULL,
+    organization_fiscal_code VARCHAR(16) NOT NULL,
+    referent_fiscal_code VARCHAR(16) NOT NULL,
     created_at TIMESTAMP WITH TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
     PRIMARY KEY (organization_fiscal_code, referent_fiscal_code),
